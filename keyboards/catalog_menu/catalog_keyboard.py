7eb487d86160faeb2e00a7458694a5207707944f
Xumpocmb <x.xumpocmb@gmail.com>
2023-09-08 14:02:16 +0300

    db start done

--- keyboards/catalog_menu/catalog_keyboard.py
+++ keyboards/catalog_menu/catalog_keyboard.py
@@ -3,12 +3,12 @@ from aiogram.types import (
 )
 
 
-button_1: InlineKeyboardButton = InlineKeyboardButton(text='Майки', callback_data='go_to_stage2')
-button_2: InlineKeyboardButton = InlineKeyboardButton(text='Шорты', callback_data='go_to_stage2')
-button_3: InlineKeyboardButton = InlineKeyboardButton(text='Жопа друга', callback_data='go_to_stage2')
-button_4: InlineKeyboardButton = InlineKeyboardButton(text='Кроссовки', callback_data='go_to_stage2')
-button_5: InlineKeyboardButton = InlineKeyboardButton(text='Полуботинки', callback_data='go_to_stage2')
-button_6: InlineKeyboardButton = InlineKeyboardButton(text='Ботинки', callback_data='go_to_stage2')
+button_1: InlineKeyboardButton = InlineKeyboardButton(text='Майки', callback_data='t_shirt')
+button_2: InlineKeyboardButton = InlineKeyboardButton(text='Шорты', callback_data='shorts')
+button_3: InlineKeyboardButton = InlineKeyboardButton(text='Жопа друга', callback_data='ass')
+button_4: InlineKeyboardButton = InlineKeyboardButton(text='Кроссовки', callback_data='sneakers')
+button_5: InlineKeyboardButton = InlineKeyboardButton(text='Полуботинки', callback_data='low_shoes')
+button_6: InlineKeyboardButton = InlineKeyboardButton(text='Ботинки', callback_data='boots')
 
 
 catalog_keyboard: InlineKeyboardMarkup = InlineKeyboardMarkup(
